e03707680f90ee97a54d3f39ed0ec8e74042155d
Bianca <bianca.koinegg@hotmail.com>
2019-03-26 08:05:19 +0100

    project setup

---
+++ persistencespring/src/main/java/at/fhv/team5/sportsfreund/persistencespring/App.java
@@ -0,0 +1,13 @@
+package at.fhv.team5.sportsfreund.persistencespring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
